5a8838af9aac2c03d56fc3c662af6cdd6d4122b8
Alex Swain <alexswain@Alexs-MBP.broadband>
2022-06-29 11:51:11 +0100

    adds new experience and portfolio sections

--- js/main.js
+++ js/main.js
@@ -30,18 +30,24 @@ function linkAction() {
 navLink.forEach(link => link.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className
+
+    for(let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+} 
+skillsHeader.forEach((header) => {
+    header.addEventListener('click', toggleSkills)
+})
 
 
-/*==================== QUALIFICATION TABS ====================*/
-
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
