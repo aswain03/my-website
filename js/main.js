0a8618ca4e527e3d5065b0a4ca046afbe07a1bdd
Alexander Swain <alexanderswain@Alexanders-MBP.broadband>
2022-06-29 17:49:39 +0100

    adds completed sections. needs nav bar fix and css various css touch ups

--- js/main.js
+++ js/main.js
@@ -51,12 +51,58 @@ skillsHeader.forEach((header) => {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]")
 
+function scrollActive() {
+    const scrollY = window.pageYOffset
 
+    sections.forEach((section) => {
+        const sectionHeight = section.offsetHeight
+        const sectionTop = section.offsetTop - 50;
+        sectionId = section.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add('active-link')
+        } else {
+            document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove('active-link')  
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById("header")
+    if(this.scrollY >=5) nav.classList.add('scroll-header'); else nav.classList.remove("scroll-header")
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
-
+function scrollTop() {
+    const scrollTop = document.getElementById("scroll-up")
+    if(this.scrollY >= 650) scrollTop.classList.add("show-scroll"); else scrollTop.classList.remove("show-scroll")
+}
+window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/ 
+const themeButton = document.getElementById("theme-button")
+const darkTheme = "dark-theme"
+const iconTheme = "uil-sunset"
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = themeButton.classList.contains("selected-theme")
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light"
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moonset' : "uil-sunset"
+
+if(selectedTheme) {
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+    themeButton.classList[selectedIcon === "uil-moonset" ? "add" : "remove"](iconTheme);
+};
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    
+    localStorage.setItem("selected-theme", getCurrentTheme())
+    localStorage.setItem("selected-icon", getCurrentIcon())
+})
